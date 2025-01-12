30d06efd4e0e132fc7ac1b1870ce582cee94adf9
Naufal Adrian <87624905+naufaladrian@users.noreply.github.com>
2025-01-12 11:58:46 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: 'http://localhost:5173',
+		origin: ['http://localhost:5173','https://landslide-report-app.vercel.app']
 		allowedHeaders: ['Content-Type', 'Authorization'],
 	})
 );
